b9cb680c0184196bbe8ca53d9c7fcf526d7f031c
Prathmesh <connect.prathmesh905@gmail.com>
2022-10-22 20:39:12 +0530

    button and typewriter text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,10 @@ const Home = () => {
       <div className=" font-bold text-black mt-[10rem] mobile:text-[40px] lg:text-[64px]">
         Speak out your <span className="text-Primary">&nbsp;Feelings</span>
       </div>
-      <div className="text-gray-500 lg:text-[20px] ">
-        Connect with a therapist to get your problems solved...
+      <div className="text-gray-500 lg:text-[20px] mt-5 flex-row flex">
+        <div>Connect with a therapist to get your&nbsp;</div><div className="type">problems solved...</div>
       </div>
+      <button className="text-white bg-Primary px-[80px] py-[15px] mt-10 rounded-md text-[25px] mb-40 hover:bg-blue-400 transition-all" >Get Help</button>
     </div>
   )
 }
